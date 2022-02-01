94820adb7159a9774912c29222b16cd895aebf83
qgustavor <qgustavor@users.noreply.github.com>
2022-02-01 12:20:07 -0300

    Animate tagline

---
+++ src/components/TagLine/index.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useRef } from 'react'
+
+export default function TagLine () {
+  const el = useRef(null)
+
+  // Animation runs using pure JavaScript and CSS for improved performance
+  // (mostly because typewriting-react was slow and making a lot of DOM changes)
+
+  useEffect(() => {
+    // Fallback for older browsers
+    if (!el.current.animate) {
+      el.current.textContent = 'Unofficial JavaScript SDK for Mega'
+      return
+    }
+
+    const originalWords = ['Node.js', 'Browser', 'Deno', 'JavaScript']
+    let words = originalWords.slice()
+    let currentWord = ''
+
+    const prefixEl = document.createElement('div')
+    const middleEl = document.createElement('div')
+    const suffixEl = document.createElement('div')
+    prefixEl.textContent = 'Unofficial'
+    middleEl.textContent = currentWord
+    suffixEl.textContent = 'SDK for Mega'
+
+    el.current.appendChild(prefixEl)
+    el.current.appendChild(middleEl)
+    el.current.appendChild(suffixEl)
+
+    let currentTimeout
+    const wait = time => new Promise(resolve => {
+      currentTimeout = setTimeout(resolve, time)
+    })
+
+    async function animationLoop () {
+      if (words.length === 0) {
+        await wait(15e3)
+        words = originalWords.slice()
+      }
+      const newWord = currentWord
+        ? ''
+        : words.shift()
+
+      // Use the FLIP technique
+      // :: First
+      const prefixBF = prefixEl.getBoundingClientRect()
+      const suffixBF = suffixEl.getBoundingClientRect()
+
+      // :: Execute
+      middleEl.textContent = newWord
+
+      // :: Last
+      const prefixBL = prefixEl.getBoundingClientRect()
+      const suffixBL = suffixEl.getBoundingClientRect()
+
+      // :: Invert
+      const prefixDelta = prefixBF.left - prefixBL.left
+      const suffixDelta = suffixBF.left - suffixBL.left
+
+      // :: Play
+      if (!newWord) middleEl.textContent = currentWord
+      currentWord = newWord
+
+      prefixEl.animate([{
+        transformOrigin: 'top left',
+        transform: `translateX(${prefixDelta}px)`
+      }, {
+        transformOrigin: 'top left',
+        transform: 'none'
+      }], {
+        duration: 300,
+        easing: 'ease-in-out',
+        fill: 'both'
+      })
+      middleEl.animate([{
+        transformOrigin: 'top center',
+        transform: newWord ? 'perspective(10vw) rotateY(-90deg)' : 'none'
+      }, {
+        transformOrigin: 'top center',
+        transform: newWord ? 'none' : 'perspective(10vw) rotateY(90deg)'
+      }], {
+        duration: 300,
+        easing: 'ease-in-out',
+        fill: 'both'
+      })
+      suffixEl.animate([{
+        transformOrigin: 'top left',
+        transform: `translateX(${suffixDelta}px)`
+      }, {
+        transformOrigin: 'top left',
+        transform: 'none'
+      }], {
+        duration: 300,
+        easing: 'ease-in-out',
+        fill: 'both'
+      })
+
+      await wait(currentWord ? 3000 : 300)
+      animationLoop()
+    }
+    animationLoop()
+
+    return () => {
+      clearTimeout(currentTimeout)
+    }
+  }, [])
+
+  return (
+    <p className='hero__subtitle flip_tagline' ref={el}></p>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,16 @@ import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import styles from './index.module.css'
 import CodeBlockSwitchable from '@site/src/components/CodeBlockSwitchable'
+import TagLine from '@site/src/components/TagLine'
 
 function HomepageHeader () {
   const { siteConfig } = useDocusaurusContext()
-
-  // TODO: cycle those sentences
-  // const sentences = ['Node.js', 'Browser', 'Deno', 'JavaScript']
 
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className='container'>
         <h1 className='hero__title'>{siteConfig.title}</h1>
-        <p className='hero__subtitle'>
-          Unofficial JavaScript SDK for Mega
-        </p>
+        <TagLine />
         <div className={styles.buttons}>
           <Link
             className='button button--secondary button--lg'
