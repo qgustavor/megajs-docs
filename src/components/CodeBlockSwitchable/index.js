fe8809707e37dd43a038c564e26e31a3874f7229
qgustavor <qgustavor@users.noreply.github.com>
2024-05-03 23:07:42 -0300

    Fix tabs

--- src/components/CodeBlockSwitchable/index.js
+++ src/components/CodeBlockSwitchable/index.js
@@ -3,19 +3,14 @@ import CodeBlock from '@theme/CodeBlock'
 import Tabs from '@theme/Tabs'
 import TabItem from '@theme/TabItem'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import { modeNames, replacementFns } from '../../plugins/node2deno/replacements.js'
+import { modes, modeNames, replacementFns } from '../../plugins/node2deno/replacements.js'
 
 export default function CodeBlockSwitchable (props) {
-  const versionSuffix = (props.version == undefined || Number(props.version) === 1) ? '' : '@' + props.version
-
   return (
     <div>
       <Tabs groupId="node2deno" className="margin-bottom--sm">{
         modeNames.map((modeName, idx) =>
-          <TabItem value={idx} label={modeName}
-            onClick={() => setMode(idx)}
-            key={'+'+ idx}
-          >
+          <TabItem value={modes[idx]} label={modeName} key={modes[idx]}>
             <CodeBlock language={props.language}>{replacementFns[idx](props.code)}</CodeBlock>
           </TabItem>
         )
