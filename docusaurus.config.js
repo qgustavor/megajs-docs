a5db113e8ffacf7b574ab7bf4bd557933af4ecb1
qgustavor <qgustavor@users.noreply.github.com>
2024-06-11 01:33:06 -0300

    Set respectPrefersColorScheme to true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true
+      },
       navbar: {
         title: 'MEGAJS',
         items: [
