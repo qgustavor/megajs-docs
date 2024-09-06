52a880a921e58605f5db3e47390e71c049990e2f
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2024-09-06 09:34:08 -0300

    Fix file-searching-demo.js
    It was missing a `.name`

--- static/demos/file-searching-demo.js
+++ static/demos/file-searching-demo.js
@@ -2,7 +2,7 @@ import { getLoggedInStorage } from './logging-in-demo.js'
 const storage = await getLoggedInStorage()
 
 console.log('Those are all .jpeg files in the storage:')
-console.log(storage.filter(e => e.endsWith('.jpeg'), true))
+console.log(storage.filter(e => e.name.endsWith('.jpeg'), true))
 
 console.log('Those are all README files in the storage:')
 console.log(storage.filter('README', true))
